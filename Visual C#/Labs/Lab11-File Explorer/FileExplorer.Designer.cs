b5d8e7f9a7ee1a8c5e2e7fe9233488012ab594c4
Karim Ali <kariimalii96@gmail.com>
2023-02-09 05:38:17 +0200

    First Panel Logic

---
+++ Visual C#/Labs/Lab11-File Explorer/FileExplorer.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Lab11_File_Explorer
+{
+    partial class FileExplorer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.listBox1);
+            this.panel1.Location = new System.Drawing.Point(199, 81);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(432, 581);
+            this.panel1.TabIndex = 0;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(3, 3);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(426, 580);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.DoubleClick += new System.EventHandler(this.listBox1_DoubleClick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(202, 32);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(429, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(754, 257);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(754, 429);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(364, 703);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(754, 703);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(1116, 703);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // FileExplorer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1543, 795);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "FileExplorer";
+            this.Text = "File Explorer";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Visual C#/Labs/Lab11-File Explorer/FileExplorer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.IO;
+namespace Lab11_File_Explorer
+{
+    public partial class FileExplorer : Form
+    {
+        string Path1;
+        public FileExplorer()
+        {
+            InitializeComponent();
+            //================ Adjust Window =================//
+            this.Width = Screen.PrimaryScreen.Bounds.Width;
+            this.Height = Screen.PrimaryScreen.Bounds.Height;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+
+            Path1 = "";
+
+            //adjustListBox(listBox1, Path1, textBox1);
+            initializeListBox(listBox1, textBox1);
+
+        }
+
+        private void listBox1_DoubleClick(object sender, EventArgs e)
+        {
+            string name = listBox1.SelectedItem.ToString();
+            if (name[name.Length - 1] == char.Parse(@"\")) Path1 = Path1 + name;
+            else Path1 = Path1 + @"\" + name;
+            adjustListBox(listBox1, Path1, textBox1);
+        }
+        public static void adjustListBox(ListBox listbox, string path, TextBox textbox)
+        {
+            listbox.Items.Clear();
+            DirectoryInfo dir = new DirectoryInfo(path);
+            DirectoryInfo[] dirs = dir.GetDirectories();
+            for (int i = 0; i < dirs.Length; i++) listbox.Items.Add(dirs[i].ToString()); // "📁 " +
+
+            FileInfo[] files = dir.GetFiles();
+            for (int i = 0; i < files.Length; i++) listbox.Items.Add(files[i].ToString());
+
+            textbox.Text = Path.GetFullPath(path);
+        }
+        public static void initializeListBox(ListBox listbox, TextBox textbox)
+        {
+
+            DriveInfo[] allDrives = DriveInfo.GetDrives();
+            for (int i = 0; i < allDrives.Length; i++) listbox.Items.Add(allDrives[i].ToString());
+            textbox.Text = "";
+        }
+    }
+}
+
+
+// RESOURCES
+// https://learn.microsoft.com/en-us/dotnet/api/system.io.path.getfullpath?view=net-7.0
+// https://www.c-sharpcorner.com/UploadFile/99aadd/working-with-the-directoryinfo-class-in-C-Sharp/
+// https://learn.microsoft.com/en-us/dotnet/api/system.io.driveinfo.getdrives?view=net-6.0
